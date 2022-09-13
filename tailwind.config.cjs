9a905359d2b7ed73c2222c8f4044e871c19a9c15
cesar-lima <cesarr340@gmail.com>
2022-09-12 23:05:50 -0300

    construcao da landing page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,17 @@ module.exports = {
     './index.html'
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['inter', 'sans-serif']
+    },
+    extend: {
+      backgroundImage: {
+        'duo-gradient' : 'linear-gradient(89.86deg, #9572FC 27.08%, #43E7AD 29.94%, #E1D55D 44.57%)',
+        'game-gradient' : 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)',
+        galaxy: "url('/background-galaxy.png')"
+      },
+
+    },
   },
   plugins: [],
 }
